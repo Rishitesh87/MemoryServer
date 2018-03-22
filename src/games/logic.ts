133ffb1a90485688fd63b52888d7e4e24b933e4b
Rishitesh87 <roepnarainr@gmail.com>
2018-03-22 09:15:27 +0100

    Changed the logic file

--- src/games/logic.ts
+++ src/games/logic.ts
@@ -1,54 +1,45 @@
 import { ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator'
-import { Board, Symbol, Row } from './entities';
+import { board} from './entities';
 
-// @ValidatorConstraint()
-// export class IsBoard implements ValidatorConstraintInterface {
-//
-//   validate(board: Board) {
-//     const symbols = [ 'x', 'o', null ]
-//     return board.length === 3 &&
-//       board.every(row =>
-//         row.length === 3 &&
-//         row.every(symbol => symbols.includes(symbol))
-//       )
-//   }
-// }
 
-// export const isValidTransition = (playerSymbol: Symbol, from: Board, to: Board) => {
-//   const changes = from
-//     .map(
-//       (row, rowIndex) => row.map((symbol, columnIndex) => ({
-//         from: symbol,
-//         to: to[rowIndex][columnIndex]
-//       }))
-//     )
-//     .reduce((a,b) => a.concat(b))
-//     .filter(change => change.from !== change.to)
-//
-//   return changes.length === 1 &&
-//     changes[0].to === playerSymbol &&
-//     changes[0].from === null
-// }
+export const identicalSquares= (board)=> {
+  const openedSquares=board.filter((Square) => {
+      Square.value === 2
+    })
 
-export const calculateWinner = (board: Board): Symbol | null =>
+  if (openedSquares[0].name === openedSquares[1].name) {
+     return true;
+   } else {
+     return false;
+   }
+}
+
+
+function totalOpenedSquares (board){
+  return board
+    .map((Square)=> {
+      return Square.value
+    })
+    .filter(value => value === 2)
+    .length
+
+}
+
+export const check = () => {
+  if (totalOpenedSquares(board)===2) {
+    if (identicalSquares){
+      //Change Square value to
+      board.filter
+    }
+    else {
+      //Change Square values to default
+    }
+  }
+
+export const finished = (board) : boolean =>
   board
-    .concat(
-      // vertical winner
-      [0, 1, 2].map(n => board.map(row => row[n])) as Row[]
-    )
-    .concat(
-      [
-        // diagonal winner ltr
-        [0, 1, 2].map(n => board[n][n]),
-        // diagonal winner rtl
-        [0, 1, 2].map(n => board[2-n][n])
-      ] as Row[]
-    )
-    .filter(row => row[0] && row.every(symbol => symbol === row[0]))
-    .map(row => row[0])[0] || null
-
-export const finished = (board: Board): boolean =>
-  board
-    .reduce((a,b) => a.concat(b) as Row)
-    .filter(symbol => symbol === null)
+    .map((Square)=> {
+      return Square.value
+    })
+    .filter(value => value === 0)
     .length === 0
